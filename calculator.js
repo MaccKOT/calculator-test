d4cf6d1591ac5e627f42c60e52abedfad3368994
MaccKOT <no@mail.com>
2024-02-28 18:37:27 +0400

    algoritm fix and optimization

--- calculator.js
+++ calculator.js
@@ -21,23 +21,17 @@ function calculator(input) {
 	if (
 		regexForRomanDigits.test(left) === true &&
 		regexForRomanDigits.test(right) === true
-	)
+	) {
 		isRoman = true;
-	else {
+		left = romans.deromanize(left);
+		right = romans.deromanize(right);
+	} else {
 		left = parseInt(left);
 		right = parseInt(right);
 		if (Number.isNaN(left) || Number.isNaN(right)) throw new Error();
-		if (left > 10 || left <= 0 || right > 10 || right <= 0) throw new Error();
 	}
 
-	if (isRoman) {
-		left = romans.deromanize(left);
-		right = romans.deromanize(right);
-		if (left > 10 || right > 10) throw new Error();
-	}
-
-	//проверка на деление на ноль
-	if (sign === "/" && right === 0) throw new Error("Divide by zero");
+	if (left > 10 || left <= 0 || right > 10 || right <= 0) throw new Error();
 
 	let result = null;
 	if (sign === "+") result = left + right;
@@ -45,7 +39,8 @@ function calculator(input) {
 	if (sign === "/") result = Math.floor(left / right);
 	if (sign === "*") result = left * right;
 
-	if (result <= 0 && isRoman) return ""; // в римских цифрах нет нуля и отрицательных чисел
+	// в римских цифрах нет нуля и отрицательных чисел
+	if (result <= 0 && isRoman) return "";
 	if (isRoman) return romans.romanize(result);
 
 	return result.toString();
@@ -53,6 +48,6 @@ function calculator(input) {
 
 export default calculator;
 
-//console.log(calculator("3 % 4"));  //error
+//console.log(calculator("3 % 4")); //error
 //console.log(calculator("10 - 1")); // 9
 //console.log(calculator("II + V")); //VII
